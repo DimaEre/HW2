967d22ce58da791ae4e65b96330de4450421e246
Дмитро Єремія <diameremia2006@gmail.com>
2022-10-15 12:34:23 +0300

    Add project files.

---
+++ HW2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+class Zad1 //Требуется сложить числа A и B
+{
+    static void Main()
+    {
+        int a;
+        string[] month = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+        Console.Write("Введите число A: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write(month[a]);
+        Console.ReadKey();
+    }
+}
